44c8d0c82901087d93cb8feeb6722f739f9a7eff
Коржикoff <off@DESKTOP-DK>
2024-09-24 13:55:08 +0300

    Добавьте файлы проекта.

---
+++ PracticalTasks6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Text;
+
+namespace PracticalTasks6
+{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<<Программа учета сотрудников>>");
+            Console.WriteLine("Если Вы хотите вывести список сотрудников на экран нажмите 1");
+            Console.WriteLine("Если Вы хотите добавить нового сотрудника нажмите 2");
+            string sType = Console.ReadLine();
+            switch (sType)
+            {
+                case "1": 
+                    Show();
+                    break;
+                case "2":
+                    AddNew();
+                    break;
+             };
+                     
+        }
+    }
+}
